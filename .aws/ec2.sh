0e21d2eaf874867be19bea769d4f7b3d4d6326c5
Arata <aratadayoon@yahoo.co.jp>
2025-04-28 15:44:34 +0900

    add: include setup and up commands in ec2.sh

--- .aws/ec2.sh
+++ .aws/ec2.sh
@@ -8,3 +8,5 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo make setup
+sudo make up
