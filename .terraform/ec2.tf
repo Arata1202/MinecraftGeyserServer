76337840f16ac6568aa5bcc1a17169268646884f
Arata <aratadayoon@yahoo.co.jp>
2025-05-06 16:58:55 +0900

    add: add egress rules to the Minecraft security group in ec2.tf

--- .terraform/ec2.tf
+++ .terraform/ec2.tf
@@ -42,6 +42,13 @@ resource "aws_security_group" "minecraft_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "minecraft_sg"
   }
