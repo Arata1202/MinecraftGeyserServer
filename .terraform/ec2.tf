fed4eff1cc09853e311947ac8ea739644f38ab2d
Arata <aratadayoon@yahoo.co.jp>
2025-05-06 15:12:36 +0900

    fix: standardize naming conventions for AWS instance resource in ec2.tf

--- .terraform/ec2.tf
+++ .terraform/ec2.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "Minecraft-server" {
+resource "aws_instance" "minecraft_server" {
   ami           = "ami-026c39f4021df9abe"
   instance_type = "t3a.medium"
-  key_name      = "Minecraft-key"
+  key_name      = "minecraft_key"
 
   root_block_device {
     volume_type = "gp3"
@@ -9,6 +9,6 @@ resource "aws_instance" "Minecraft-server" {
   }
 
   tags = {
-    Name = "Minecraft-server"
+    Name = "minecraft_server"
   }
 }
