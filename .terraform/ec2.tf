cac69cea77784417351e4b6ee93955fc4d0ed89c
Arata <aratadayoon@yahoo.co.jp>
2025-05-06 15:33:00 +0900

    add: define AWS security group for Minecraft server and update instance configuration

--- .terraform/ec2.tf
+++ .terraform/ec2.tf
@@ -1,7 +1,10 @@
 resource "aws_instance" "minecraft_server" {
-  ami           = "ami-026c39f4021df9abe"
-  instance_type = "t3a.medium"
-  key_name      = "minecraft_key"
+  ami                         = "ami-026c39f4021df9abe"
+  instance_type               = "t3a.medium"
+  key_name                    = "minecraft_key"
+  subnet_id                   = aws_subnet.minecraft_public_subnet_1.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.minecraft_sg.id]
 
   root_block_device {
     volume_type = "gp3"
@@ -10,5 +13,36 @@ resource "aws_instance" "minecraft_server" {
 
   tags = {
     Name = "minecraft_server"
+  }
+}
+
+resource "aws_security_group" "minecraft_sg" {
+  name        = "minecraft_sg"
+  description = "minecraft_sg"
+  vpc_id      = aws_vpc.minecraft_vpc.id
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 19132
+    to_port     = 19132
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "minecraft_sg"
   }
 }
