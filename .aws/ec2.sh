49f36b6171fb46dde8209068b62491d83d72751e
Arata <aratadayoon@yahoo.co.jp>
2025-05-10 22:29:50 +0900

    fix: install mcrcon in Dockerfile and ec2.sh, update docker-compose.yml ports

--- .aws/ec2.sh
+++ .aws/ec2.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install -y ca-certificates curl gnupg make mcrcon
+sudo apt install -y ca-certificates curl gnupg make git gcc
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
 echo \
@@ -8,3 +8,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+git clone https://github.com/Tiiffi/mcrcon.git
+cd mcrcon
+make
+sudo cp mcrcon /usr/local/bin
