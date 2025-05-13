0759e0fe3a1e4c531ce2b797e153d79b5ff5b05c
Arata <aratadayoon@yahoo.co.jp>
2025-05-13 18:00:19 +0900

    feat: add user data script for Minecraft server instance initialization

--- .terraform/ec2.tf
+++ .terraform/ec2.tf
@@ -7,6 +7,35 @@ resource "aws_instance" "minecraft_server" {
   vpc_security_group_ids      = [aws_security_group.minecraft_sg.id]
   iam_instance_profile        = aws_iam_instance_profile.minecraft_instance_profile.name
 
+  user_data = <<-EOF
+    Content-Type: multipart/mixed; boundary="//"
+    MIME-Version: 1.0
+
+    --//
+    Content-Type: text/cloud-config; charset="us-ascii"
+    MIME-Version: 1.0
+    Content-Transfer-Encoding: 7bit
+    Content-Disposition: attachment; filename="cloud-config.txt"
+
+    # cloud-config
+    cloud_final_modules:
+    - [scripts-user, always]
+
+    --//
+    Content-Type: text/x-shellscript; charset="us-ascii"
+    MIME-Version: 1.0
+    Content-Transfer-Encoding: 7bit
+    Content-Disposition: attachment; filename="userdata.txt"
+
+    # !/bin/bash
+
+    if [ -d "/home/ubuntu/MinecraftGeyserServer" ]; then
+      cd /home/ubuntu/MinecraftGeyserServer
+      make up
+    fi
+    --//
+  EOF
+
   root_block_device {
     volume_type = var.volume_type
     volume_size = var.volume_size
