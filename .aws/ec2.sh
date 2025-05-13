58eb774103174c66a22dedc2ecbbec082b512ec9
Arata <aratadayoon@yahoo.co.jp>
2025-05-13 10:06:30 +0900

    fix: update AWS CLI download URL to use architecture-specific variable

--- .aws/ec2.sh
+++ .aws/ec2.sh
@@ -12,6 +12,6 @@ git clone https://github.com/Tiiffi/mcrcon.git
 cd mcrcon
 make
 sudo cp mcrcon /usr/local/bin
-curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
