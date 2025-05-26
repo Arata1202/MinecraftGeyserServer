cd9210d41c4f64041502b6bf039d771be9ae5926
Arata <aratadayoon@yahoo.co.jp>
2025-05-26 11:56:26 +0900

    refactor: remove unused Lambda function files for c7i and t3a, and streamline start.py for instance management

--- lambda/start.py
+++ lambda/start.py
@@ -10,54 +10,29 @@ def lambda_handler(event, context):
     region = os.environ.get('AWS_REGION', 'ap-northeast-1')
     webhook_url = os.environ['WEBHOOK_URL']
 
+    instance_type = 't3a.medium'
+    java_memory = '2G'
+    spec_description = '低スペック'
+
+    if 'queryStringParameters' in event and event['queryStringParameters']:
+        params = event['queryStringParameters']
+        if 'type' in params:
+            req_type = params['type'].lower()
+            if req_type == 'c7i':
+                instance_type = 'c7i.xlarge'
+                java_memory = '6G'
+                spec_description = '高スペック'
+
+
+    desired_type = instance_type
+
     ssm = boto3.client('ssm', region_name=region)
     ec2 = boto3.client('ec2', region_name=region)
 
-    ec2.start_instances(InstanceIds=[instance_id])
+    instance = ec2.describe_instances(InstanceIds=[instance_id])
+    state = instance['Reservations'][0]['Instances'][0]['State']['Name']
 
-    while True:
-        instance = ec2.describe_instances(InstanceIds=[instance_id])
-        state = instance['Reservations'][0]['Instances'][0]['State']['Name']
-        if state == 'running':
-            break
-        time.sleep(5)
-
-    while True:
-        statuses = ec2.describe_instance_status(InstanceIds=[instance_id])
-        if statuses['InstanceStatuses']:
-            inst_status = statuses['InstanceStatuses'][0]['InstanceStatus']['Status']
-            sys_status = statuses['InstanceStatuses'][0]['SystemStatus']['Status']
-            if inst_status == 'ok' and sys_status == 'ok':
-                break
-        time.sleep(5)
-
-    response = ssm.send_command(
-        InstanceIds=[instance_id],
-        DocumentName="AWS-RunShellScript",
-        Parameters={'commands': ["cd /home/ubuntu/MinecraftGeyserServer && sudo make up"]},
-    )
-
-    command_id = response['Command']['CommandId']
-
-    while True:
-        try:
-            result = ssm.get_command_invocation(
-                CommandId=command_id,
-                InstanceId=instance_id
-            )
-            if result['Status'] in ['Success', 'Cancelled', 'TimedOut', 'Failed']:
-                break
-        except ClientError as e:
-            if "InvocationDoesNotExist" in str(e):
-                time.sleep(1)
-                continue
-            else:
-                raise e
-        time.sleep(1)
-
-    if result['Status'] != 'Success':
-        message = f"make up に失敗しました⚠️"
-    else:
+    if state == 'running':
         instance_type = instance['Reservations'][0]['Instances'][0]['InstanceType']
 
         if instance_type.startswith('c7i'):
@@ -67,7 +42,68 @@ def lambda_handler(event, context):
         else:
             spec_note = ''
 
-        message = f'サーバーの起動が完了しました🟢\nインスタンスタイプ：{instance_type} {spec_note}'
+        message = f'サーバーはすでに起動しています🟢\nインスタンスタイプ：{instance_type} {spec_note}'
+    else:
+        ec2.modify_instance_attribute(
+            InstanceId=instance_id,
+            InstanceType={'Value': desired_type}
+        )
+
+        ec2.start_instances(InstanceIds=[instance_id])
+
+        while True:
+            instance = ec2.describe_instances(InstanceIds=[instance_id])
+            state = instance['Reservations'][0]['Instances'][0]['State']['Name']
+            if state == 'running':
+                break
+            time.sleep(5)
+
+        while True:
+            statuses = ec2.describe_instance_status(InstanceIds=[instance_id])
+            if statuses['InstanceStatuses']:
+                inst_status = statuses['InstanceStatuses'][0]['InstanceStatus']['Status']
+                sys_status = statuses['InstanceStatuses'][0]['SystemStatus']['Status']
+                if inst_status == 'ok' and sys_status == 'ok':
+                    break
+            time.sleep(5)
+
+        response_script = ssm.send_command(
+            InstanceIds=[instance_id],
+            DocumentName="AWS-RunShellScript",
+            Parameters={'commands': [
+                f'echo "java -Xmx{java_memory} -Xms{java_memory} -jar Folia.jar" > /home/ubuntu/MinecraftGeyserServer/.bin/start.sh',
+                'chmod +x /home/ubuntu/MinecraftGeyserServer/.bin/start.sh'
+            ]},
+        )
+
+        response = ssm.send_command(
+            InstanceIds=[instance_id],
+            DocumentName="AWS-RunShellScript",
+            Parameters={'commands': ["cd /home/ubuntu/MinecraftGeyserServer && sudo make up"]},
+        )
+
+        command_id = response['Command']['CommandId']
+
+        while True:
+            try:
+                result = ssm.get_command_invocation(
+                    CommandId=command_id,
+                    InstanceId=instance_id
+                )
+                if result['Status'] in ['Success', 'Cancelled', 'TimedOut', 'Failed']:
+                    break
+            except ClientError as e:
+                if "InvocationDoesNotExist" in str(e):
+                    time.sleep(1)
+                    continue
+                else:
+                    raise e
+            time.sleep(1)
+
+        if result['Status'] != 'Success':
+            message = f"make up に失敗しました⚠️"
+        else:
+            message = f'サーバーの起動が完了しました🟢\nインスタンスタイプ：{desired_type} （{spec_description}）'
 
     data = json.dumps({'content': message}).encode('utf-8')
 
