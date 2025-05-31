41f8e458e7aa89be07efc60f7ad6f2a536c7f15a
Arata <aratadayoon@yahoo.co.jp>
2025-05-31 13:35:37 +0900

    feat: add ingress rule for port 8123 in security group and update Dockerfile and docker-compose for new port exposure

--- .terraform/ec2/sg.tf
+++ .terraform/ec2/sg.tf
@@ -24,6 +24,13 @@ resource "aws_security_group" "minecraft_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8123
+    to_port     = 8123
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
