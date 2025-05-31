e23cbe255675bb713a328ace72a85ba04d9fb15a
Arata <aratadayoon@yahoo.co.jp>
2025-05-31 22:19:26 +0900

    feat: update entrypoint script to copy plugin jars from local jar directory

--- .bin/update.sh
+++ .bin/update.sh
@@ -1,22 +1,8 @@
-VERSION=$(curl -s "https://api.papermc.io/v2/projects/paper" | jq -r '.versions[-1]')
-BUILD=$(curl -s "https://api.papermc.io/v2/projects/paper/versions/${VERSION}" | jq -r '.builds[-1]')
-curl -f -L -o Paper.jar \
-    "https://api.papermc.io/v2/projects/paper/versions/${VERSION}/builds/${BUILD}/downloads/paper-${VERSION}-${BUILD}.jar"
-
-curl -f -L -o plugins/Geyser-Spigot.jar \
-    "https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot"
-
-curl -f -L -o plugins/Floodgate-Spigot.jar \
-    "https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot"
-
-curl -f -L -o plugins/Via-Version.jar \
-    "https://api.spiget.org/v2/resources/19254/download"
-
-curl -f -L -o plugins/DiscordSRV.jar \
-    "https://download.discordsrv.com/v2/DiscordSRV/DiscordSRV/release/download/latest/jar"
-
-curl -f -L -o LunaChat.jar \
-    "https://api.spiget.org/v2/resources/82293/download"
-
-curl -f -L -o DeathChest.jar \
-    "https://api.spiget.org/v2/resources/101066/download"
+cp ./jar/Paper.jar ./
+cp ./jar/Geyser-Spigot.jar plugins/
+cp ./jar/Floodgate-Spigot.jar plugins/
+cp ./jar/Via-Version.jar plugins/
+cp ./jar/DiscordSRV.jar plugins/
+cp ./jar/LunaChat.jar plugins/
+cp ./jar/DeathChest.jar plugins/
+cp ./jar/Dynmap.jar plugins/
