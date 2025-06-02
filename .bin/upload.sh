ab6a39b716e7a6e972867bb935e0b5f44dc329be
Arata <aratadayoon@yahoo.co.jp>
2025-06-02 12:37:12 +0900

    feat: remove server.properties upload from upload script

--- .bin/upload.sh
+++ .bin/upload.sh
@@ -16,8 +16,3 @@ if [ -d "./uploads/world_the_end" ]; then
     rsync -e "ssh -i $KEY_PATH" -az --delete ./uploads/world_the_end/ ubuntu@$IP_ADDRESS:/home/ubuntu/MinecraftGeyserServer/world_the_end/
     rm -rf ./uploads/world_the_end
 fi
-
-if [ -f "./uploads/server.properties" ]; then
-    rsync -e "ssh -i $KEY_PATH" -az ./uploads/server.properties ubuntu@$IP_ADDRESS:/home/ubuntu/MinecraftGeyserServer/server.properties
-    rm ./uploads/server.properties
-fi
