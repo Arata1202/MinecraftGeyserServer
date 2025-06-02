067ae102a47c9f550eba4156bda785701baa99fa
Arata <aratadayoon@yahoo.co.jp>
2025-06-02 22:48:22 +0900

    feat: update security group to allow traffic on ports 80 and 443

--- .terraform/ec2/sg.tf
+++ .terraform/ec2/sg.tf
@@ -25,8 +25,15 @@ resource "aws_security_group" "minecraft_sg" {
   }
 
   ingress {
-    from_port   = 8123
-    to_port     = 8123
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
