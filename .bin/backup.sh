a3c46153eea179f9d57eea77e1e0a55a43feb5a9
Arata <aratadayoon@yahoo.co.jp>
2025-06-02 12:30:40 +0900

    feat: simplify backup script to include all files and exclude .git directory

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -7,11 +7,7 @@ set +a
 BACKUP_NAME="minecraft_backup_$(date +%Y%m%d_%H%M%S).tar.gz"
 TMP_PATH="/tmp/$BACKUP_NAME"
 
-tar -czf "$TMP_PATH" \
-    world \
-    world_nether \
-    world_the_end \
-    server.properties
+tar --exclude='.git' -czf "$TMP_PATH" .
 
 aws s3 cp "$TMP_PATH" "s3://$BUCKET_NAME/backups/$BACKUP_NAME"
 
