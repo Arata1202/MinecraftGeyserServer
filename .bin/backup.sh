034f80e502c56f166e7ae0d8756d2f5df644d152
Arata <aratadayoon@yahoo.co.jp>
2025-06-02 23:45:31 +0900

    Revert "feat: simplify backup script to include all files and exclude .git directory"
    This reverts commit a3c46153eea179f9d57eea77e1e0a55a43feb5a9.

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -7,7 +7,11 @@ set +a
 BACKUP_NAME="minecraft_backup_$(date +%Y%m%d_%H%M%S).tar.gz"
 TMP_PATH="/tmp/$BACKUP_NAME"
 
-tar --exclude='.git' -czf "$TMP_PATH" .
+tar -czf "$TMP_PATH" \
+    world \
+    world_nether \
+    world_the_end \
+    server.properties
 
 aws s3 cp "$TMP_PATH" "s3://$BUCKET_NAME/backups/$BACKUP_NAME"
 
