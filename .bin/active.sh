77283e090eb3997a22f6b45b3f177fd28c906f75
Arata <aratadayoon@yahoo.co.jp>
2025-06-03 17:42:00 +0900

    feat: add batch service with Dockerfile and active.py for log monitoring

--- .bin/active.sh
+++ .bin/active.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-set -a
-source /home/ubuntu/MinecraftGeyserServer/.env
-set +a
-
-python3 /home/ubuntu/MinecraftGeyserServer/.bin/active.py >> /home/ubuntu/MinecraftGeyserServer/logs/active.log 2>&1
+cd /home/ubuntu/MinecraftGeyserServer
+docker compose run --rm batch
