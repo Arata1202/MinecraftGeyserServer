7329ba8f97129cb836a895c133354bdb2c0b8074
Arata <aratadayoon@yahoo.co.jp>
2025-06-05 23:36:00 +0900

    feat: replace start.sh with cmd.sh for Minecraft server execution

--- lambda/start.py
+++ lambda/start.py
@@ -71,8 +71,8 @@ def lambda_handler(event, context):
             InstanceIds=[instance_id],
             DocumentName="AWS-RunShellScript",
             Parameters={'commands': [
-                f'echo "java -Xmx{java_memory} -Xms{java_memory} -jar Paper.jar" > /home/ubuntu/MinecraftGeyserServer/.bin/start.sh',
-                'chmod +x /home/ubuntu/MinecraftGeyserServer/.bin/start.sh'
+                f'echo "java -Xmx{java_memory} -Xms{java_memory} -jar Paper.jar" > /home/ubuntu/MinecraftGeyserServer/.docker/minecraft/cmd.sh',
+                'chmod +x /home/ubuntu/MinecraftGeyserServer/.docker/minecraft/cmd.sh'
             ]},
         )
 
