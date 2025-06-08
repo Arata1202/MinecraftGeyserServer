475377d17a9cf33914730c771260438aa60b3b75
Arata <aratadayoon@yahoo.co.jp>
2025-06-08 10:49:53 +0900

    fix: add sudo to docker compose command in stop.sh

--- .bin/stop.sh
+++ .bin/stop.sh
@@ -4,7 +4,7 @@ set -a
 source .env
 set +a
 
-docker compose exec minecraft \
+sudo docker compose exec minecraft \
   mcrcon -H 127.0.0.1 -P 25575 -p ${RCON_PASSWORD} stop
 
 while pgrep -f "java.*Paper.jar" > /dev/null; do
